4e4562a102563cd5db59e34b15a4b7a52ddca1e1
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-02-24 08:50:09 -0800

    initial commit

---
+++ PierresBakery/Models/Bread.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  public class Bread
+  {
+    // properties, constructors, methods, etc. go here
+  }
+  
+}
---
+++ PierresBakery/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PierresBakery
+{
+  class Program
+  {
+    static void Main()
+    {
+
+    }
+  }
+}
