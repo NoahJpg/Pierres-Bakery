d2499c4a356b289c8e99d3a52329a1c11a94ac9c
Noah Atkinson <noahatkinson1.1@gmail.com>
2023-02-24 15:53:52 -0800

    update UI design with background and text colors

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -10,37 +10,51 @@ namespace PierresBakery
   {
     static void Main(string[] args)
     {
-      string[] lines = {@"  __                                     __                         
- /  | /                     /          |/  |      /                 
-(___|   ___  ___  ___  ___     ___     |___| ___ (     ___  ___     
-|    | |___)|   )|   )|___)   |___     |   )|   )|___)|___)|   )   )
-|    | |__  |    |    |__      __/     |__/ |__/|| \  |__  |    \_/ 
-                                                                 /  ", "Welcome to Pierre's Bakery!", "We have the BEST dough in the Northern Hemisphere!", "And a great sale on Bread and Pastries right now.", "Bread is $5 each or Buy 2, get 1 free.", "Pastries are $2 each or Buy 3, get 1 free."};
+      Console.BackgroundColor = ConsoleColor.White;
+      Console.ForegroundColor = ConsoleColor.Black;
+      string[] lines = 
+      { @"
+        __                                     __                         
+       /  | /                     /          |/  |      /                 
+      (___|   ___  ___  ___  ___     ___     |___| ___ (     ___  ___     
+      |    | |___)|   )|   )|___)   |___     |   )|   )|___)|___)|   )   )
+      |    | |__  |    |    |__      __/     |__/ |__/|| \  |__  |    \_/ 
+                                                                      /  ", 
+      "                        Welcome to Pierre's Bakery!", 
+      "    ",
+      " We have the BEST dough in the Northern Hemisphere!", 
+      " And a great sale on Bread and Pastries right now.", 
+      " Bread is $5 each or Buy 2, get 1 free.", 
+      " Pastries are $2 each or Buy 3, get 1 free."
+      };
       foreach (string line in lines)
       {
         Console.WriteLine(line);
         Thread.Sleep(1500);
       }
-
-      Console.WriteLine("How many loaves of Bread would you like today? :)  ");
+      
+      Console.WriteLine(" How many loaves of Bread would you like today? :)  ");
       int breadQuantity = int.Parse(Console.ReadLine());
 
-      Console.WriteLine("How many pastries would you like? ");
+      Console.WriteLine(" How many pastries would you like? ");
       int pastryQuantity = int.Parse(Console.ReadLine());
 
-      Console.WriteLine("How many Cinnamon Rolls would you like today?");
+      Console.WriteLine(" How many Cinnamon Rolls would you like today?");
       int cinnamonRollQuantity = int.Parse(Console.ReadLine());
 
       CinnamonRoll cinnamonRoll = new CinnamonRoll(cinnamonRollQuantity);
       Bread bread = new Bread(breadQuantity);
       Pastry pastry = new Pastry(pastryQuantity);
 
+      Console.ForegroundColor = ConsoleColor.Green;
       string[] lines2 = 
       {
-      $"Cost for {breadQuantity} loaves of Bread: ${bread.CalculatePrice()}", 
-      $"Cost for {pastryQuantity} Pastries: ${pastry.CalculatePrice()}", 
-      $"Cost for {cinnamonRollQuantity} Cinnamon Rolls: ${cinnamonRoll.CalculatePrice()}", 
-      $"Total cost for your order: ${bread.CalculatePrice() + pastry.CalculatePrice() + cinnamonRoll.CalculatePrice()}"
+      $" Cost for {breadQuantity} loaves of Bread: ${bread.CalculatePrice()}", 
+      $" Cost for {pastryQuantity} Pastries: ${pastry.CalculatePrice()}", 
+      $" Cost for {cinnamonRollQuantity} Cinnamon Rolls: ${cinnamonRoll.CalculatePrice()}", 
+      $" Total cost for your order: ${bread.CalculatePrice() + pastry.CalculatePrice() + cinnamonRoll.CalculatePrice()}",
+      " Thanks for coming in today.", 
+      " Hope to see you again soon!"
       };
       foreach (string line in lines2)
       {
